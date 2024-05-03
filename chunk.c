6ab3dbb391cfc4609694cda53d1e0a7c3ebb40cc
Erick 光 <erickcestari03@gmail.com>
2024-05-03 15:24:54 -0300

    feat: add lines

--- chunk.c
+++ chunk.c
@@ -11,17 +11,21 @@ void initChunk(Chunk *chunk)
   initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte)
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
 {
+
   if (chunk->capacity < chunk->count + 1)
   {
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code,
                              oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines,
+                              oldCapacity, chunk->capacity);
   }
 
   chunk->code[chunk->count] = byte;
+  chunk->lines[chunk->count] = line;
   chunk->count++;
 }
 
@@ -34,6 +38,7 @@ int addConstant(Chunk *chunk, Value value)
 void freeChunk(Chunk *chunk)
 {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  FREE_ARRAY(int, chunk->lines, chunk->capacity);
   freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
