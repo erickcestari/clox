09c8bc1c04e9eba47a10903909a936faf2c34fdd
Erick 光 <erickcestari03@gmail.com>
2024-05-07 11:45:56 -0300

    feat: add vm

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[])
 {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
 
@@ -14,6 +16,8 @@ int main(int argc, const char *argv[])
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
+  interpret(&chunk);
+  freeVM();
 
   return 0;
 }
