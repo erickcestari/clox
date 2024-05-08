5c5973150507f5d2603da03d368858e95fb4d6cf
Erick 光 <erickcestari03@gmail.com>
2024-05-08 14:00:50 -0300

    feat: add scanner

--- vm.c
+++ vm.c
@@ -4,6 +4,7 @@
 #include "debug.h"
 #include "value.h"
 #include "memory.h"
+#include "compiler.h"
 
 VM vm;
 
@@ -107,9 +108,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+  compile(source);
+  return INTERPRET_OK;
 }
