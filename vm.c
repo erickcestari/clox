9e5963bc3ef2e16998ce47b228f045c5b217fc6b
Erick 光 <erickcestari03@gmail.com>
2024-05-08 09:37:23 -0300

    feat: add binary operation

--- vm.c
+++ vm.c
@@ -35,6 +35,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do                  \
+  {                   \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;)
   {
@@ -60,6 +67,21 @@ static InterpretResult run()
       push(constant);
       break;
     }
+    case OP_ADD:
+      BINARY_OP(+);
+      break;
+    case OP_SUBTRACT:
+      BINARY_OP(-);
+      break;
+    case OP_MULTIPLY:
+      BINARY_OP(*);
+      break;
+    case OP_DIVIDE:
+      BINARY_OP(/);
+      break;
+    case OP_NEGATE:
+      push(-pop());
+      break;
     case OP_RETURN:
     {
       printValue(pop());
@@ -71,6 +93,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
