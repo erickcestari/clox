c848790e276e6422cc5a30000a5b9d2a85f9e8e6
Erick 光 <erickcestari03@gmail.com>
2024-06-24 10:18:32 -0300

    feat: add simple garbage collector

--- vm.c
+++ vm.c
@@ -1,11 +1,15 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+
 #include "common.h"
 #include "vm.h"
 #include "debug.h"
 #include "value.h"
 #include "memory.h"
 #include "compiler.h"
+#include "object.h"
+#include "memory.h"
 
 VM vm;
 
@@ -32,11 +36,13 @@ void initVM()
 {
   vm.stack = NULL;
   vm.stackCapacity = 0;
+  vm.objects = NULL;
   resetStack();
 }
 
 void freeVM()
 {
+  freeObjects();
 }
 
 void push(Value value)
@@ -67,6 +73,21 @@ static Value peek(int distance)
 static bool isFalsey(Value value)
 {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate()
+{
+  ObjString *b = AS_STRING(pop());
+  ObjString *a = AS_STRING(pop());
+
+  int length = a->length + b->length;
+  char *chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString *result = takeString(chars, length);
+  push(OBJ_VAL(result));
 }
 
 static InterpretResult run()
@@ -133,8 +154,25 @@ static InterpretResult run()
       BINARY_OP(BOOL_VAL, <);
       break;
     case OP_ADD:
-      BINARY_OP(NUMBER_VAL, +);
+    {
+      if (IS_STRING(peek(0)) && IS_STRING(peek(1)))
+      {
+        concatenate();
+      }
+      else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1)))
+      {
+        double b = AS_NUMBER(pop());
+        double a = AS_NUMBER(pop());
+        push(NUMBER_VAL(a + b));
+      }
+      else
+      {
+        runtimeError(
+            "Operands must be two numbers or two strings.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
       break;
+    }
     case OP_SUBTRACT:
       BINARY_OP(NUMBER_VAL, -);
       break;
--- vm.h
+++ vm.h
@@ -13,6 +13,7 @@ typedef struct
   Value *stack;
   int stackCount;
   int stackCapacity;
+  Obj *objects;
 } VM;
 
 typedef enum
@@ -21,6 +22,8 @@ typedef enum
   INTERPRET_COMPILE_ERROR,
   INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
