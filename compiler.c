eb88f73fceecff037fd609881f6bb50b6dd8f0f7
erickcestari <erickcestari03@gmail.com>
2024-07-13 09:35:16 -0300

    feat: add locals variables

--- compiler.c
+++ compiler.c
@@ -41,8 +41,19 @@ typedef struct
   Precedence precedence;
 } ParseRule;
 
-Parser parser;
+typedef struct {
+  Token name;
+  int depth;
+} Local;
 
+typedef struct {
+  Local locals[UINT8_COUNT];
+  int localCount;
+  int scopeDepth;
+} Compiler;
+
+Parser parser;
+Compiler* current = NULL;
 Chunk *compilingChunk;
 
 static Chunk *currentChunk()
@@ -140,6 +151,12 @@ static uint8_t makeConstant(Value value)
 static void emitConstant(Value value)
 {
   emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void initCompiler(Compiler* compiler) {
+    compiler->localCount = 0;
+    compiler->scopeDepth = 0;
+    current = compiler;
 }
 
 static void endCompiler()
@@ -332,6 +349,8 @@ static void expression()
 bool compile(const char *source, Chunk *chunk)
 {
   initScanner(source);
+  Compiler compiler;
+  initCompiler(&compiler);
   compilingChunk = chunk;
   parser.hadError = false;
   parser.panicMode = false;
